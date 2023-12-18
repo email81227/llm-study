de52904670a570a160a1d62c53216f31b793b525
ding <email81227@gmail.com>
2023-12-19 01:13:53 +0800

    init llm-study

---
+++ examples/test-openai.py
@@ -0,0 +1,18 @@
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv('.env')
+
+client = OpenAI(api_key=os.environ.get("openai_api_key"))
+
+response = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
+    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+    {"role": "user", "content": "Where was it played?"}
+  ]
+)
