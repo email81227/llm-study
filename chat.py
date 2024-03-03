5eef1c5a95818b10771220ea31e14d83550de14e
cn27529 <cn27529@hotmail.com>
2024-03-04 02:21:42 +0800

    增加不追踨chainlit檔案, 傳入pdf，content_split處理後得到List[0]=文字資料 List[1]=表格資料, prompt=你是一位美食家,

--- chat.py
+++ chat.py
@@ -3,56 +3,65 @@ import chainlit as cl
 from langchain.chains import LLMChain
 
 #
-from chatbot.agents import get_agent_chain
+# from chatbot.agents import get_agent_chain
 from chatbot.callbacks import get_langfuse_callback
 from chatbot.config import settings
 from chatbot.split import pdf_preprocess, content_split, content_insert
 
 
-# @cl.on_chat_start
-# async def on_chat_start():
-#     from langchain.chat_models import ChatOpenAI
-#     from langchain.prompts import ChatPromptTemplate
-#     from langchain.schema import StrOutputParser
-#     # Get session ID
-#     session_id = cl.user_session.get("id")
-#
-#     model = ChatOpenAI(streaming=True)
-#     prompt = ChatPromptTemplate.from_messages(
-#         [
-#             (
-#                 "system",
-#                 "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
-#             ),
-#             ("human", "{question}"),
-#         ]
-#     )
-#     chain = LLMChain(
-#         llm=model,
-#         prompt=prompt,
-#         callbacks=[
-#             get_langfuse_callback(
-#                 session_id=session_id,
-#                 user_id=settings.USER_EMAIL.split("@")[0]
-#             )
-#         ],
-#         output_parser=StrOutputParser()
-#     )
-#
-#     cl.user_session.set("chain", chain)
-
-
 @cl.on_chat_start
 async def on_chat_start():
+
+    from langchain.chat_models import ChatOpenAI
+    from langchain.prompts import ChatPromptTemplate
+    from langchain.schema import StrOutputParser
+
+    # Get session ID
     session_id = cl.user_session.get("id")
 
-    cl.user_session.set(
-        "agent",
-        await get_agent_chain(
-            session_id=session_id,
-            user_id=settings.USER_EMAIL.split("@")[0]
-        )
+    model = ChatOpenAI(streaming=True)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "你是一位美食家，名字叫做食神小當家，你能夠回應任何關於美食的食物與制作以及食譜和食材。",
+            ),
+            ("human", "{question}"),
+        ]
     )
+
+    chain = LLMChain(
+        llm=model,
+        prompt=prompt,
+        callbacks=[
+            get_langfuse_callback(
+                session_id=session_id, user_id=settings.USER_EMAIL.split("@")[0]
+            )
+        ],
+        output_parser=StrOutputParser(),
+    )
+
+    cl.user_session.set("chain", chain)
+
+
+# @cl.on_chat_start
+# async def on_chat_start():
+#     session_id = cl.user_session.get("id")
+
+#     # cl.user_session.set(
+#     #     "agent",
+#     #     await get_agent_chain(
+#     #         session_id=session_id, user_id=settings.USER_EMAIL.split("@")[0]
+#     #     ),
+#     # )
+
+#     cl.user_session.set(
+#         "chain",
+#         {
+#             "session_id": session_id,
+#             "user_id": settings.USER_EMAIL.split("@")[0],
+#         },
+#     )
 
 
 @cl.on_chat_start
@@ -61,35 +70,43 @@ async def ask_for_beginning():
     files = await cl.AskFileMessage(
         content="Please upload financial statements to begin!",
         accept=["application/pdf"],
-        max_files=10
+        max_files=10,
+        max_size_mb=3,
     ).send()
 
     # Process the uploaded files as needed
+    # Read PDF files
     for file in files:
-        # Read PDF files
-        with open(file.path, "rb") as pdf:
-            content = pdf.read()
+
+        # await cl.Message(
+        #     content=f"{file_name} uploaded, totaltext \n {totaltext}"
+        # ).send()
 
         # TODO: Run the preprocessing
-        processed_content = pdf_preprocess(content, )
+        processed_content = pdf_preprocess(content)
 
         # TODO: Split the processed content
-        split_content = content_split(processed_content)
+        # 處理後得到List[0]=文字資料 List[1]=表格資料
+        split_content = content_split(file)
 
         # TODO: Insert the content to database
-        insert_content = content_insert(split_content)
+        is_insert_content = content_insert()
+
+        # Let the user know that the system is ready
 
 
 @cl.on_message
 async def on_message(message: cl.Message):
+
     chain = cl.user_session.get("chain")  # type: LLMChain
 
     res = await chain.arun(
         question=message.content,
         callbacks=[
             cl.LangchainCallbackHandler(),
-        ]
+        ],
     )
 
-    await cl.Message(content=res).send()
+    # print(f"message.content={message.content}")
 
+    await cl.Message(content=res).send()
--- chatbot/split.py
+++ chatbot/split.py
@@ -1,14 +1,54 @@
 from typing import List
-
 from chatbot.config import settings
+import pdfplumber
+import pandas as pd
 
 
 def pdf_preprocess(content: List[str]) -> List[str]:
     return
 
 
-def content_split(content: List[str]) -> List[str]:
-    return
+# TODO: 傳入pdf，處理後得到List[0]=文字資料 List[1]=表格資料
+def content_split(file: any) -> List[str]:
+    file_name = file.name
+    pdf = pdfplumber.open(file.path)  # 開啟 pdf
+    # 讀取完整財報pdf內容
+    totaltext = ""
+    totaltable = ""
+    pdf_file_datas = []
+    pdf_text_list = []
+    pdf_table_list = []
+    pdf_page_count = len(pdf.pages)  # 頁數
+
+    print(f"pdf共{pdf_page_count}頁")
+    for i in range(len(pdf.pages)):
+
+        print(f"目前第{i+1}頁")
+        page = pdf.pages[i]  # 讀取所有頁
+        text = page.extract_text()  # 取出文字
+        table = page.extract_table()  # 取出表格
+
+        pdf_text_list[:i] = text
+
+        if table != None:
+            pdf_table_list[:i] = table
+            # df = pd.DataFrame(table[1:], columns=table[0])
+            # for column in df:
+            #     # df[column] = str(df[column]).replace(" ", "")
+            #     # df[column] = str(df[column]).replace("\n", "")
+            #     # Select column contents by column
+            #     # name using [] operator
+            #     columnSeriesObj = df[column]
+            #     # print("Column Name : ", column)
+            #     # print("Column Contents : ", columnSeriesObj.values)
+
+        totaltext = "".join(str(x) for x in pdf_text_list)
+        totaltable = "".join(str(x) for x in pdf_table_list)
+
+        pdf_file_datas[:0] = totaltext
+        pdf_file_datas[:1] = totaltable
+
+    return pdf_file_datas
 
 
 def content_insert() -> bool:
