16cc9411999509420511f64d9801e90d9fb4c3a3
Yi-Ding Lei <yi.ding.lei@zenpulsar.com>
2024-03-05 19:14:26 +0800

    Add some prompt example for the python tools

--- chatbot/prompts/py_tool_prompts.py
+++ chatbot/prompts/py_tool_prompts.py
@@ -1,5 +1,10 @@
 
-PY_COMPUTE_PROMPT = "Enter something here"
+PY_COMPUTE_PROMPT = (
+    "This works for answering the statistics, correlation between sequences, and doing calculations."
+)
 
-PY_PLOT_PROMPT = "Enter something here"
+PY_PLOT_PROMPT = (
+    "Useful when matching any following conditions:\n"
+    "1. The user ask to visualise the data points from sources or provided.\n"
+)
 
