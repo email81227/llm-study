d961b72006973b53733ce355fae219f4b9ae44ef
leia0 <leia840419@gmail.com>
2024-03-12 22:17:39 +0800

    0312-1017

--- learning/TreatPDF.py
+++ learning/TreatPDF.py
@@ -8,30 +8,27 @@ _ = load_dotenv(find_dotenv()) # read local .env file
 
 openai.api_key  = os.environ['OPENAI_API_KEY']
 
-# Load PDF
-import os
-#os.chdir('/')  # Colab 換路徑使用
 
-import pdfplumber
-file_path= "./202302_2330_AI1_20240309_163214.pdf"
-docs = pdfplumber.open(file_path)   # 開啟 pdf
-print(docs.pages)  #顯示頁數
+#載入文件
+from langchain.document_loaders import PyPDFLoader
+loader = PyPDFLoader("202302_2330_AI1_20240309_163214.pdf")
+pages = loader.load()
+#print(pages)
 
-#讀取完整財報pdf內容
-toteltext = ''
-toteltable= ''
 
-for i in range(len(docs.pages)):
-  page = docs.pages[i]           # 讀取所有頁
-  text = page.extract_text()    # 取出文字
-  table = page.extract_table()  # 取出表格
-  if table != None:
-    toteltable += str(table) 
-  toteltext += text 
+#longchain splitting
+from langchain.text_splitter import TokenTextSplitter
 
+text_splitter = TokenTextSplitter(chunk_size=500, chunk_overlap=0)
+docs = text_splitter.split_documents(pages)
+print(len(docs))
+print(docs[1],pages[1].metadata)
+
+'''
 #打印
 path = 'output.txt'
 f = open(path, 'w')
 print(toteltext, file=f)
 print('pdf解析表格'+toteltable, file=f)
 f.close()
+'''
