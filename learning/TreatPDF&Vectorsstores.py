963edf1902ce33ef9f3e441c69ccf990ddde5628
leia0 <leia840419@gmail.com>
2024-03-17 14:49:56 +0800

    0317-1449

--- learning/TreatPDF&Vectorsstores.py
+++ learning/TreatPDF&Vectorsstores.py
@@ -9,9 +9,17 @@ _ = load_dotenv(find_dotenv()) # read local .env file
 
 openai.api_key  = os.environ['OPENAI_API_KEY']
 
+import datetime
+current_date = datetime.datetime.now().date()
+if current_date < datetime.date(2023, 9, 2):
+    llm_name = "gpt-3.5-turbo-0301"
+else:
+    llm_name = "gpt-3.5-turbo"
+print(llm_name)
+
 
 #載入文件
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 loader = PyPDFLoader("202302_2330_AI1_20240309_163214.pdf")
 pages = loader.load()
 #print(pages)
@@ -26,31 +34,16 @@ print(len(docs))
 #print(docs[1],pages[1].metadata)
 
 
-#Embeddings
+def pretty_print_docs(docs):
+    print(f"\n{'-' * 100}\n".join([f"Document {i+1}:\n\n" + d.page_content for i, d in enumerate(docs)]))
+
+# 建立vector db
+from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
 
 embedding = OpenAIEmbeddings()
-
-sentence1 = "第幾年度第幾季財報"
-sentence2 = "i like canines"
-sentence3 = "the weather is ugly outside"
-
-embedding1 = embedding.embed_query(sentence1)
-embedding2 = embedding.embed_query(sentence2)
-embedding3 = embedding.embed_query(sentence3)
-
-
-#Vectorstores
-from langchain.vectorstores import Chroma
-persist_directory = 'docs/chroma/'
-
-vectordb = Chroma.from_documents(
-    documents=docs,
-    embedding=embedding,
-    persist_directory=persist_directory
-)
-vectordb.persist()
-print(vectordb._collection.count())
+vectordb = Chroma.from_documents(documents=docs, embedding=embedding)
+retriever_vector = vectordb.as_retriever()
 
 
 #索引方式-compression上下文壓縮
@@ -67,8 +60,6 @@ compression_retriever = ContextualCompressionRetriever(
     base_retriever=vectordb.as_retriever()
 )
 
-question = "what did they say about matlab?"
+question = "這財報介紹了哪些內容？"
 compressed_docs = compression_retriever.get_relevant_documents(question)
 print(compressed_docs)
-
-
